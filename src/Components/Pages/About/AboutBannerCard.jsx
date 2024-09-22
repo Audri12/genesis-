81a98b834a9d12c20e0f7e427ecb439e2cf269f9
Mashrufa Mehnaz <mashrufamehnaz@gmail.com>
2024-09-22 23:01:59 +0600

    n

---
+++ src/Components/Pages/About/AboutBannerCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AboutBannerCard = () => {
+  return (
+    <div>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default AboutBannerCard
